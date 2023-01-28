6c4378aebd1dc51d8c65289c4c03184bad9bc216
Engels Antero <engelsantero@gmail.com>
2023-01-28 14:24:41 -0300

    Update app.js

--- App 1 - ES6/app.js
+++ App 1 - ES6/app.js
@@ -9,10 +9,41 @@ class Despesa{
 		this.valor = valor
 	}
 
+	validarDados(){
+		for(let i in this){
+			if(this[i] === undefined || this[i] === '' || this[i] === null)
+				return false
+		}
+
+		return true
+	}
+
 }
 
+class Bd {
 
+	constructor(){
+		let id = localStorage.getItem('id')
 
+		if(id == null){
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoID(){
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoID()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id', id)
+	}
+
+}
+
+let bd = new Bd()
 
 function CadastrarDespesa() {
 	
@@ -29,12 +60,25 @@ function CadastrarDespesa() {
 		descricao.value, 
 		valor.value)
 
-	gravar(despesa)
+	if(despesa.validarDados()){
+		bd.gravar(despesa)
 
-}
+		document.getElementById('cabecalhoModal').className = 'modal-header text-success'
+		document.getElementById('gravacaoLabel').innerHTML = 'Despesa cadastrada com sucesso'
+		document.getElementById('botaoModal').className = 'btn btn-success'
+		document.getElementById('conteudoModal').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('botaoModal').innerHTML = 'Voltar'
 
-function gravar(d) {
+		$('#modalRegistraDespesa').modal('show')
+	}else{
 
-	localStorage.setItem('despesa', JSON.stringify(d))
+		document.getElementById('cabecalhoModal').className = 'modal-header text-danger'
+		document.getElementById('botaoModal').className = 'btn btn-danger'
+		document.getElementById('gravacaoLabel').innerHTML = 'Erro ao registrar nova despesa'
+		document.getElementById('conteudoModal').innerHTML = 'É necessario preencher todos os campos para adicionar uma despesa a lista'
+		document.getElementById('botaoModal').innerHTML = 'Voltar e corrigir'
+
+		$('#modalRegistraDespesa').modal('show')
+	}
 
 }
